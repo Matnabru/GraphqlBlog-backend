d798b7fa1eb5adb70e94efeaf0e7f772429bf20e
Mateusz Urbanek <matnabru@gmail.com>
2021-11-28 23:45:30 +0100

    CORS functionality

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,15 @@ import { schema } from './graphql/schemas/schema'
 import { resolvers } from "./graphql/resolvers/resolvers"
 const mongoose = require('mongoose')
 dotenv.config()
-
+var cors = require('cors');
 
 const path = require('path')
 const PORT = process.env.PORT || 8000
 
-
+console.log('Frontend side set to : ' + process.env.FRONTEND_LINK)
 
 const app = express()
-
+app.use(cors({credentials: true, origin: process.env.FRONTEND_LINK}));
 app.use('/graphql', graphqlHTTP({
     schema:schema,
     rootValue:resolvers,
@@ -27,5 +27,5 @@ const options = {useNewUrlParser: true, useUnifiedTopology: true}
 // MONGODB
 mongoose.connect(uri, options).then(
   app.listen(PORT,),
-  console.log(`Listening on ${ PORT }`)
+  console.log(`Listening on http://localhost:${ PORT }`)
 )
